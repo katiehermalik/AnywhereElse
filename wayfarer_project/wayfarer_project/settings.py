94ae74c818e7f5734f7ce2e5770556f9fb17ca67
khermalik <khermalik@gmail.com>
2020-12-31 18:14:10 -0800

    test for deployment

--- wayfarer_project/wayfarer_project/settings.py
+++ wayfarer_project/wayfarer_project/settings.py
@@ -32,10 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://anywhere-else-app.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
